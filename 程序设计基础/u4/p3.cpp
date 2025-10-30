4b3197bea5e820880c71ccebbabbf3b0b604f53a
RWAYICN <69195867+rwayicn@users.noreply.github.com>
2025-10-30 04:51:39 +0000

    new file:   "\347\250\213\345\272\217\350\256\276\350\256\241\345\237\272\347\241\200/u4/p3.cpp"

---
+++ 程序设计基础/u4/p3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int rn(int y);
+
+int main()
+{
+    int y,m,d;
+    cin>>y>>m>>d;
+    if (y )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int rn(int y)
+{
+    if (y % 400 == 0 || (y % 100 != 0 && y % 4 == 0))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
