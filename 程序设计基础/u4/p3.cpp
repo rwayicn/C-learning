cbccfd19446be34dadcc6c649a9f927389baf8e9
RWAYICN <69195867+rwayicn@users.noreply.github.com>
2025-10-30 05:48:41 +0000

    modified:   "\347\250\213\345\272\217\350\256\276\350\256\241\345\237\272\347\241\200/u4/p3.cpp"

--- 程序设计基础/u4/p3.cpp
+++ 程序设计基础/u4/p3.cpp
@@ -3,17 +3,18 @@
 using namespace std;
 
 int rn(int y);
+int dc(int y,int m,int d);
 
 int main()
 {
-    int y,m,d;
+    int y,m,d,md;
     cin>>y>>m>>d;
-    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) md=31
-    else if (m == 4 || m == 6 || m == 9 || m == 11) md=30
+    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) md=31;
+    else if (m == 4 || m == 6 || m == 9 || m == 11) md=30;
     else 
     {
-        if (rn(y) == 1) md=29
-        else md=28
+        if (rn(y) == 1) md=29;
+        else md=28;
     }
 }
 
