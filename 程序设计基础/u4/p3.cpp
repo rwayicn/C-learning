de1b148da296d7595bf06d725e916396e9ec4688
RWAYICN <69195867+rwayicn@users.noreply.github.com>
2025-10-30 05:35:24 +0000

    modified:   "\347\250\213\345\272\217\350\256\276\350\256\241\345\237\272\347\241\200/u4/p3.cpp"

--- 程序设计基础/u4/p3.cpp
+++ 程序设计基础/u4/p3.cpp
@@ -8,7 +8,7 @@ int main()
 {
     int y,m,d;
     cin>>y>>m>>d;
-    if (y )
+    if (y >= 0 && ((m == 1 || m == 3 || m ==5 || m == 7 || m == 8 || m == 10 || m == 12) && d >= 1 && d <= 31) &&)
 }
 
 
@@ -25,6 +25,49 @@ int main()
 
 
 
+
+int dc(int y,int m,int d)
+{
+    int sum;
+    if (rn(y) == 1)
+    {
+        switch(m)
+        {
+            case 1:sum = d;break;
+            case 2:sum = 31 + d;break;
+            case 3:sum = 60 + d;break;
+            case 4:sum = 91 + d;break;
+            case 5:sum = 121 + d;break;
+            case 6:sum = 152 + d;break;
+            case 7:sum = 183 + d;break;
+            case 8:sum = 214 + d;break;
+            case 9:sum = 244 + d;break;
+            case 10:sum = 274 + d;break;
+            case 11:sum = 305 + d;break;
+            case 12:sum = 335 + d;break;
+        }
+        return sum;
+    }
+    else
+    {
+        switch(m)
+        {
+            case 1:sum = d;break;
+            case 2:sum = 31 + d;break;
+            case 3:sum = 59 + d;break;
+            case 4:sum = 90 + d;break;
+            case 5:sum = 120 + d;break;
+            case 6:sum = 151 + d;break;
+            case 7:sum = 182 + d;break;
+            case 8:sum = 213 + d;break;
+            case 9:sum = 243 + d;break;
+            case 10:sum = 273 + d;break;
+            case 11:sum = 304 + d;break;
+            case 12:sum = 334 + d;break;
+        }
+    }
+    return sum;
+}
 
 int rn(int y)
 {
