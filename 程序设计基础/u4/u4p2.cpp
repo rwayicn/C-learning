60d424ac246fbd72e1c5e04eaa299d8a358690a4
RWAYICN <69195867+rwayicn@users.noreply.github.com>
2025-10-30 01:53:11 +0000

    modified:   "\347\250\213\345\272\217\350\256\276\350\256\241\345\237\272\347\241\200/u4/u4p2.cpp"

--- 程序设计基础/u4/u4p2.cpp
+++ 程序设计基础/u4/u4p2.cpp
@@ -20,13 +20,20 @@ int main()
         case 5 :t = n * 0.35 - 5505;break;
         case 6 :t = n * 0.45 - 13505;break;
     }
-    cout<<"全月应缴税额为"<<fixed<<setprecision(2)<<t<<"元"<<endl;
+    if (t > 0)
+    {
+        cout<<"全月应缴税额为"<<fixed<<setprecision(2)<<t<<"元"<<endl;
+    }
+    else
+    {
+        cout<<"全月应缴税额为0.00元"<<endl;
+    }
     return 0;
 }
 
 int level(long double m)
 {
-    if (m <= 1500)
+    if (m != 0 && m <= 1500)
     {
         return 0;
     }
